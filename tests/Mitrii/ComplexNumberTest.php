bcc3b8a6d9a8dc57fc6bef3986041dceec1ba496
mitrii <p.mitrii@gmail.com>
2021-01-28 00:22:49 +0600

    add test

--- tests/Mitrii/ComplexNumberTest.php
+++ tests/Mitrii/ComplexNumberTest.php
@@ -7,4 +7,33 @@ use PHPUnit\Framework\TestCase;
 class ComplexNumberTest extends TestCase
 {
 
+    public function test__toString()
+    {
+        $complex = new ComplexNumber(1, 2, 2);
+        $this->assertSame((string) $complex, '1.00+2.00i');
+
+        $complex = new ComplexNumber(3, 4, 4);
+        $this->assertSame((string) $complex, '3.0000+4.0000i');
+    }
+
+    public function testMul()
+    {
+
+    }
+
+    public function testDiv()
+    {
+
+    }
+
+
+    public function testAdd()
+    {
+
+    }
+
+    public function testSub()
+    {
+
+    }
 }
