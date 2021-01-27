acb71ec459c5912cdd5bed550e61caac8b039ac6
mitrii <p.mitrii@gmail.com>
2021-01-28 00:22:35 +0600

    add constructor and base methods

--- src/ComplexNumber.php
+++ src/ComplexNumber.php
@@ -5,4 +5,42 @@ namespace Mitrii;
 class ComplexNumber
 {
 
+    private float $re;
+
+    private float $im;
+
+    private int $precision;
+
+    public function __construct(float $re, float $im, $precision = 2)
+    {
+        $this->re = $re;
+        $this->im = $im;
+        $this->precision = $precision;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf("%0.{$this->precision}f%+0.{$this->precision}fi", $this->re, $this->im);
+    }
+
+    public function add($value): ComplexNumber
+    {
+
+    }
+
+
+    public function sub($value): ComplexNumber
+    {
+
+    }
+
+    public function div($value): ComplexNumber
+    {
+
+    }
+
+    public function mul($value): ComplexNumber
+    {
+
+    }
 }
