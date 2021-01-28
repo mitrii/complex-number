a6d1a69f64d0c3e38570e4585bd467e1c93d4b3e
mitrii <p.mitrii@gmail.com>
2021-01-28 11:53:31 +0600

    add addiction operation

--- src/ComplexNumber.php
+++ src/ComplexNumber.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Mitrii;
 
@@ -11,7 +12,7 @@ class ComplexNumber
 
     private int $precision;
 
-    public function __construct(float $re, float $im, $precision = 2)
+    public function __construct(float $re, float $im, int $precision = 2)
     {
         $this->re = $re;
         $this->im = $im;
@@ -20,25 +21,68 @@ class ComplexNumber
 
     public function __toString(): string
     {
-        return sprintf("%0.{$this->precision}f%+0.{$this->precision}fi", $this->re, $this->im);
+        return sprintf("%0.{$this->precision}f%+0.{$this->precision}fi", $this->getRe(), $this->getIm());
     }
 
+    public function getRe(): float
+    {
+        return $this->re;
+    }
+
+    public function getIm(): float
+    {
+        return $this->im;
+    }
+
+    /**
+     * @param ComplexNumber|int|float $value
+     * @return ComplexNumber
+     */
+    protected function normalize($value): ComplexNumber
+    {
+        if ($value instanceof self) {
+            return $value;
+        }
+
+        return new ComplexNumber((float) $value, 0, $this->precision);
+    }
+
+    /**
+     * @param ComplexNumber|int|float $value
+     * @return ComplexNumber
+     */
     public function add($value): ComplexNumber
     {
+        $complexValue = $this->normalize($value);
 
+        $re = $this->getRe() + $complexValue->getRe();
+        $im = $this->getIm() + $complexValue->getIm();
+
+        return new ComplexNumber($re, $im, $this->precision);
     }
 
-
+    /**
+     * @param ComplexNumber|int|float $value
+     * @return ComplexNumber
+     */
     public function sub($value): ComplexNumber
     {
 
     }
 
+    /**
+     * @param ComplexNumber|int|float $value
+     * @return ComplexNumber
+     */
     public function div($value): ComplexNumber
     {
 
     }
 
+    /**
+     * @param ComplexNumber|int|float $value
+     * @return ComplexNumber
+     */
     public function mul($value): ComplexNumber
     {
 
