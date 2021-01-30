e7956170d497224ccc910d8e9a7d395c5bb61bbd
mitrii <p.mitrii@gmail.com>
2021-01-31 00:23:00 +0600

    simplified trigonometric tests

--- tests/Mitrii/Complex/TrigonometricExporterTest.php
+++ tests/Mitrii/Complex/TrigonometricExporterTest.php
@@ -9,12 +9,16 @@ class TrigonometricExporterTest extends TestCase
 
     public function testExport()
     {
+        $complex = new Number(0,0);
+        $exporter = new TrigonometricExporter();
+        self::assertSame('0', $exporter->export($complex));
+
         $complex = new Number(-1,-4);
         $exporter = new TrigonometricExporter();
-        self::assertSame('sqrt(17.00)*(cos(pi+arctg(4.00))+isin(pi+arctg(4.00)))', $exporter->export($complex));
+        self::assertSame('sqrt(17.00)*(cos(phi)+isin(phi))', $exporter->export($complex));
 
         $complex = new Number(-2,4);
         $exporter = new TrigonometricExporter();
-        self::assertSame('sqrt(20.00)*(cos(pi+arctg(-2.00))+isin(pi+arctg(-2.00)))', $exporter->export($complex));
+        self::assertSame('sqrt(20.00)*(cos(phi)+isin(phi))', $exporter->export($complex));
     }
 }
