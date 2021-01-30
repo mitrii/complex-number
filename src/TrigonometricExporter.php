a227ced47c6899ce3e555bd08d5dae55f0750204
mitrii <p.mitrii@gmail.com>
2021-01-30 22:40:38 +0600

    add trigonometric exporter

---
+++ src/TrigonometricExporter.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Mitrii\Complex;
+
+
+class TrigonometricExporter implements ExporterInterface
+{
+    private function getR(Number $number)
+    {
+        return sprintf("sqrt(%0.{$number->getPrecision()}f", sqrt($number->getRe()**2 + $number->getIm()**2));
+    }
+
+    private function getFi(Number $number): string
+    {
+       return '';
+    }
+
+    public function export(Number $number): string
+    {
+        return sprintf('%s*(cos(%s)+isin(%s))', $this->getR($number), $this->getFi(), $this->getFi());
+    }
+}
