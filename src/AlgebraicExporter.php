a1cc21cf9cf00dc322938289fd7e90591087f165
mitrii <p.mitrii@gmail.com>
2021-01-31 00:23:50 +0600

    algebraic form for zero

--- src/AlgebraicExporter.php
+++ src/AlgebraicExporter.php
@@ -8,6 +8,10 @@ class AlgebraicExporter implements ExporterInterface
 {
     public function export(Number $number): string
     {
+        if ($number->getRe() === 0.0 && $number->getIm() === 0.0) {
+            return '0';
+        }
+
         return sprintf("%0.{$number->getPrecision()}f%+0.{$number->getPrecision()}fi", $number->getRe(), $number->getIm());
     }
 }
